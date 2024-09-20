280ef7e318c1b5bdd0c9c89063a618680ed7ae41
Kascencio <152246092+Kascencio@users.noreply.github.com>
2024-09-20 01:03:21 -0600

    Update server.js

--- server.js
+++ server.js
@@ -13,7 +13,8 @@ const db = mysql.createConnection({
   host: process.env.MYSQL_HOST,
   user: process.env.MYSQL_USER,
   password: process.env.MYSQL_PASSWORD,
-  database: process.env.MYSQL_DATABASE
+  database: process.env.MYSQL_DATABASE,
+  connectTimeout: 10000
 });
 
 db.connect((err) => {
