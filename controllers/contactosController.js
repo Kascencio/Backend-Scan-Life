1d874f11e72db333c6baf82d319c55c8d173c467
Kascencio <152246092+Kascencio@users.noreply.github.com>
2024-09-20 09:47:55 -0600

    Update contactosController.js

--- controllers/contactosController.js
+++ controllers/contactosController.js
@@ -5,7 +5,8 @@ const db = mysql.createConnection({
   host: process.env.MYSQL_HOST,
   user: process.env.MYSQL_USER,
   password: process.env.MYSQL_PASSWORD,
-  database: process.env.MYSQL_DATABASE
+  database: process.env.MYSQL_DATABASE,
+  connectTimeout: 10000
 });
 
 exports.agregarContacto = (req, res) => {
